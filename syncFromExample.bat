ace50939f8018c7c0150fc5ec462589e9c4ce3a4
Harry Gordon <harrygordon1000@gmail.com>
2021-10-22 09:44:56 +0100

    Sync script now syncs from U9 example to package and example

--- syncFromExample.bat
+++ syncFromExample.bat
@@ -1,6 +1,7 @@
 :: Copies the plugin contents from Example to the package. Assumed workflow is that all changes and testing happens in Example
 :: and will need copied over to the package project.
 
-Xcopy /E /I /Y .\Wholething.FallbackImagePickerProperty.Example\App_Plugins\FallbackImagePicker\ .\Wholething.FallbackImagePickerProperty\Plugins\FallbackImagePicker\
+Xcopy /E /I /Y .\Wholething.FallbackImagePickerProperty.Examples.Umbraco9\App_Plugins\FallbackImagePicker\ .\Wholething.FallbackImagePickerProperty\App_Plugins\FallbackImagePicker\
+Xcopy /E /I /Y .\Wholething.FallbackImagePickerProperty.Examples.Umbraco9\App_Plugins\FallbackImagePicker\ .\Wholething.FallbackImagePickerProperty.Example\App_Plugins\FallbackImagePicker\
 ECHO Copied all files from Example plugin folder to package plugin folder
 PAUSE

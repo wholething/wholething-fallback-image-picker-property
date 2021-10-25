761a28f92ae7cd8ef4c6a1938ebe68a2f4f8c140
Harry Gordon <harrygordon1000@gmail.com>
2021-10-25 10:48:54 +0100

    Removed examples

--- syncFromExample.bat
+++
@@ -1,7 +0,0 @@
-:: Copies the plugin contents from Example to the package. Assumed workflow is that all changes and testing happens in Example
-:: and will need copied over to the package project.
-
-Xcopy /E /I /Y .\Wholething.FallbackImagePickerProperty.Examples.Umbraco9\App_Plugins\FallbackImagePicker\ .\Wholething.FallbackImagePickerProperty\App_Plugins\FallbackImagePicker\
-Xcopy /E /I /Y .\Wholething.FallbackImagePickerProperty.Examples.Umbraco9\App_Plugins\FallbackImagePicker\ .\Wholething.FallbackImagePickerProperty.Example\App_Plugins\FallbackImagePicker\
-ECHO Copied all files from Example plugin folder to package plugin folder
-PAUSE
